110cbc7dee575b081a397ec3def25e9861a36577
Script Raccoon <scriptraccoon@gmail.com>
2023-02-05 18:06:44 +0100

    sample request

--- client/src/App.svelte
+++ client/src/App.svelte
@@ -1,1 +1,13 @@
+<script lang="ts">
+	async function handle_click() {
+		const response = await fetch("/question");
+		if (response.ok) {
+			const data = await response.json();
+			window.alert(data);
+		}
+	}
+</script>
+
 <h1>Hi there!</h1>
+
+<button on:click={handle_click}>Get answer</button>
--- server.js
+++ server.js
@@ -6,4 +6,9 @@ app.listen(PORT, () => {
 	console.log("server is running on port", PORT);
 });
 
+app.get("/question", (req, res) => {
+	console.log("request on server");
+	res.status(200).send("42");
+});
+
 app.use(express.static("client/dist"));
